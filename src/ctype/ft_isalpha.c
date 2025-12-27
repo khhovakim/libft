d665d450304601e6878b7702061b60b7de239199
Khachik Hovakimyan <khovakim@c2r16s1.42yerevan.am>
2025-12-27 16:45:49 +0400

    style(ctype): ft_isalpha Replace magic numbers with char literals.

---
+++ src/ctype/ft_isalpha.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khovakim <khovakim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/10 15:20:39 by khovakim          #+#    #+#             */
+/*   Updated: 2025/12/27 16:41:07 by khovakim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// ft_isalpha.c
+// Checks for alphabetic characters: 'A' to 'Z' and 'a' to 'z'.
+// Returns non-zero if c is an alphabetic character, zero otherwise.
+
+int	ft_isalpha(int c)
+{
+	return ( (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') );
+}

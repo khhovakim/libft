9cdfcad37274058f7a19b9a4ff292ebd9ed4bc28
Khachik Hovakimyan <khovakim@c2r18s2.42yerevan.am>
2026-01-16 19:09:08 +0400

    Deleted unit tests

--- unit_tests/include/tests.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-// Test the ft_ctype module
-void test_ft_ctype_module();
-
-// Test the ft_io module
-void test_ft_io_module();
--- unit_tests/main.cc
+++
@@ -1,7 +0,0 @@
-#include "tests.h"
-
-int main() {
-	test_ft_ctype_module();
-	test_ft_io_module();
-	return 0;
-}
